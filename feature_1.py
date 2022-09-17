b842fccb86d5e59919b54d118bb1d823ac33e2d0
nathania4c <nathania4c@gmail.com>
2022-09-16 22:49:48 -0700

    Second commit on feature-1

---
+++ feature_1.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+import feature_2
+
+def main():
+    start = datetime.now()
+    feature_2.main()
+    end = datetime.now()
+    processing_time = (end-start).total_seconds() * 1000
+    print(f"Start time: {start}.")
+    print(f"End time: {end}.")
+    print(f"Processing time: {processing_time} seconds.")
+
+if __name__ == "__main__" : 
+    main()
